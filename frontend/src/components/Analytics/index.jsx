e7dcf18fcfe611834d34a1f5803c0ec62d8a8636
Priyanshu  Padeliya <79414782+priyanshu30122001@users.noreply.github.com>
2024-09-02 01:11:58 +0530

    Update index.jsx

--- frontend/src/components/Analytics/index.jsx
+++ frontend/src/components/Analytics/index.jsx
@@ -38,7 +38,7 @@ function Analytics() {
   const handleShare =(id)=>{
    const Link = `https://quizzie-nine-lake.vercel.app/${id}`;
    navigator.clipboard.writeText(Link)
-    navigate("/quiz/${id}")
+    navigate(`/quiz/${id}`)
    notify()
   }
   return (
