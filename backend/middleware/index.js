4f6fa266b3ec4789184c9c8fb2851700ba8f19af
priyanshu30122001 <priyanshupadeliya27@gmail.com>
2024-09-01 17:19:57 +0530

    edited router and frontend

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
-const User = require("../schemas/user");
+const User = require("../schemas/User.js");
 
 // Auth Middleware 
 
